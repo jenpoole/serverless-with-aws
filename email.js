3125b207d4a3753e49ecc26af692a5a1389f60e9
jen <contact.vandepool@gmail.com>
2018-06-13 22:42:14 -0400

    *** get origin URL for response headers from request ***

--- email.js
+++ email.js
@@ -1,4 +1,19 @@
+const Busboy = require('busboy');
+
 module.exports.handler = (event, context, callback) => {
+  // PARSE MULTIPART DATA FROM REQUEST
+  // get content type header
+  const contentType = event.headers['content-type'] || event.headers['Content-Type']
+
+  // initialize a new busboy instance
+  // constructor config settings: headers of the incoming request
+  const busboy = new Busboy({ headers: {'content-type': contentType} });
+
+  // get origin URL
+  let origin = event['headers']['origin'];
+  let amp_source = event['queryStringParameters']['__amp_source_origin'];
+
+
   // CREATE A RESPONSE
   const lambdaResponse = {
     statusCode: 200,
@@ -6,12 +21,12 @@ module.exports.handler = (event, context, callback) => {
       "Content-Type": "application/json",
       "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
       "Access-Control-Allow-Methods": "POST",
-      "Access-Control-Allow-Origin": "http://localhost:4444",
+      "Access-Control-Allow-Origin": origin,
       "Access-Control-Allow-Credentials": "true",
       "Access-Control-Expose-Headers": "AMP-Access-Control-Allow-Source-Origin",
-      "AMP-Access-Control-Allow-Source-Origin": "http://localhost:4444"
+      "AMP-Access-Control-Allow-Source-Origin": amp_source
     },
-    body: JSON.stringify({"result": "hey"})
+    body: JSON.stringify({"origin": origin, "amp_source": amp_source})
   };
 
   // callback(null, lambdaResponse);
